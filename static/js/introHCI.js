50812fa15eddd2caab3705cfa627d821c6e5963d
yacoubod <yacoubod@gmail.com>
2017-01-19 10:50:17 -0800

    added Javascript update

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -17,3 +17,11 @@ function initializePage() {
 	// example: $("#div-id").click(functionToCall);
   $("a.thumbnail").click(projectClick);
 }
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
